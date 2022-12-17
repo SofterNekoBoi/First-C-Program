4014121a3c51e3989f3e8ebfc0e2fe6afccbc08a
SofterNekoBoi <120261165+SofterNekoBoi@users.noreply.github.com>
2022-12-17 12:14:16 -0500

    Add files via upload

---
+++ fuckstogive.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "How many fucks do I have to give?" << endl;
+	int fuckstogive = 0;
+	cout << fuckstogive << endl;
+	if (fuckstogive == 0) {
+		cout << "I really don't care!" << endl;
+	}
+	else if (fuckstogive > 0) {
+		cout << "I guess I do care..."
+	}
+	return 0;
+}
+
+/*
+variable++;
+variable--;
+*/
